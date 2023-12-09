cdc14857461679f3253b97c8dd1ec509cb699780
Sahas <sahasvivek001@gmail.com>
2023-12-09 15:32:01 +0530

    feat: icons

---
+++ src/util/getData.js
@@ -0,0 +1,15 @@
+const getData = async () => {
+    try {
+        const response = await fetch(process.env.REACT_APP_API_KEY); //  API endpoint
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+};
+
+
+
+export { getData };
